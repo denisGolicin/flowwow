5f3dd771999685b38cea95e2d398a20e8ab6dbbd
denisGolicin <den.golicin65@gmail.com>
2023-03-13 21:38:34 +0200

    commit

--- js/pages.js
+++ js/pages.js
@@ -58,7 +58,7 @@ function pageMove(pageid){
 
         imgFlower.src = "src/flowers/" + id + ".png";
         modelView.src = "src/models/" + id + ".glb";
-        modelView.iosSrc = "src/models/" + id + ".usdz";
+        modelView.iosSrc = "src/models/" + id + ".reality";
 
         if(id === 5){
             modelView.environmentImage = 'src/models/assets/' + id + '.hdr';
